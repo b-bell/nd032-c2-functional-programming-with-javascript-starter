8c332591ae06c56fd5bf890995242cbd1fa802d3
b--bell <babell1302@gmail.com>
2021-07-01 21:39:47 -0500

    feat: Complete manipulation of character data

--- functional_js_syntax/combined_practice.js
+++ functional_js_syntax/combined_practice.js
@@ -1,95 +1,95 @@
 const characters = [
-  {
-    name: 'Marvin the Paranoid Android',
-    role: 'First Mate',
-    universe: 'Hitchhikers Guide to the Galaxy',
-    weapon: 'severe depression',
-    power_level: 1000
-  },
-  {
-    name: 'Jabba the Hut',
-    role: 'villain',
-    universe: 'Star Wars',
-    weapon: 'henchmen',
-    power_level: 200
-  },
-  {
-    name: 'Zoë Alleyne Washburne',
-    role: 'First Mate',
-    universe: 'Firefly',
-    weapon: 'Winchester Model 1892 rifle',
-    power_level: 160
-  },
-  {
-    name: 'Peter Venkman',
-    role: 'Ghostbuster',
-    universe: 'Ghostbusters',
-    weapon: 'proton pack',
-    power_level: 120
-  },
-  {
-    name: 'Kathryn Janeway',
-    role: 'Captain',
-    universe: 'Star Trek',
-    weapon: 'Wit',
-    power_level: 140
-  },
-  {
-    name: 'Dr. Daniel Jackson',
-    role: 'Archeologist',
-    universe: 'Stargate',
-    weapon: 'Zat gun',
-    power_level: 120
-  },
-  {
-    name: 'Q',
-    role: 'God/Eternal',
-    universe: 'Star Trek',
-    weapon: 'Whatever he wants',
-    power_level: 1000
-  },
-  {
-    name: 'Boba Fett',
-    role: 'Bounty Hunter',
-    universe: 'Star Wars',
-    weapon: 'EE-3 carbine rifle',
-    power_level: 400
-  },
-  {
-    name: 'Yoda',
-    role: 'Jedi Master',
-    universe: 'Star Wars',
-    weapon: 'The Force',
-    power_level: 900
-  },
-  {
-    name: 'Mal Reynolds',
-    role: 'Captain',
-    universe: 'Firefly',
-    weapon: 'pistol',
-    power_level: 160
-  },
-  {
-    name: 'Spock',
-    role: 'First Mate',
-    universe: 'Star Trek',
-    weapon: 'Logic',
-    power_level: 170
-  },
-  {
-    name: 'R2-D2',
-    role: 'Ship`s Robot',
-    universe: 'Star Wars',
-    weapon: 'Data Probe',
-    power_level: 250
-  },
-  {
-    name: 'Lore',
-    role: 'Villain',
-    universe: 'Star Trek',
-    weapon: 'Intellect',
-    power_level: 800
-  },
+	{
+		name: 'Marvin the Paranoid Android',
+		role: 'First Mate',
+		universe: 'Hitchhikers Guide to the Galaxy',
+		weapon: 'severe depression',
+		power_level: 1000
+	},
+	{
+		name: 'Jabba the Hut',
+		role: 'villain',
+		universe: 'Star Wars',
+		weapon: 'henchmen',
+		power_level: 200
+	},
+	{
+		name: 'Zoë Alleyne Washburne',
+		role: 'First Mate',
+		universe: 'Firefly',
+		weapon: 'Winchester Model 1892 rifle',
+		power_level: 160
+	},
+	{
+		name: 'Peter Venkman',
+		role: 'Ghostbuster',
+		universe: 'Ghostbusters',
+		weapon: 'proton pack',
+		power_level: 120
+	},
+	{
+		name: 'Kathryn Janeway',
+		role: 'Captain',
+		universe: 'Star Trek',
+		weapon: 'Wit',
+		power_level: 140
+	},
+	{
+		name: 'Dr. Daniel Jackson',
+		role: 'Archeologist',
+		universe: 'Stargate',
+		weapon: 'Zat gun',
+		power_level: 120
+	},
+	{
+		name: 'Q',
+		role: 'God/Eternal',
+		universe: 'Star Trek',
+		weapon: 'Whatever he wants',
+		power_level: 1000
+	},
+	{
+		name: 'Boba Fett',
+		role: 'Bounty Hunter',
+		universe: 'Star Wars',
+		weapon: 'EE-3 carbine rifle',
+		power_level: 400
+	},
+	{
+		name: 'Yoda',
+		role: 'Jedi Master',
+		universe: 'Star Wars',
+		weapon: 'The Force',
+		power_level: 900
+	},
+	{
+		name: 'Mal Reynolds',
+		role: 'Captain',
+		universe: 'Firefly',
+		weapon: 'pistol',
+		power_level: 160
+	},
+	{
+		name: 'Spock',
+		role: 'First Mate',
+		universe: 'Star Trek',
+		weapon: 'Logic',
+		power_level: 170
+	},
+	{
+		name: 'R2-D2',
+		role: 'Ship`s Robot',
+		universe: 'Star Wars',
+		weapon: 'Data Probe',
+		power_level: 250
+	},
+	{
+		name: 'Lore',
+		role: 'Villain',
+		universe: 'Star Trek',
+		weapon: 'Intellect',
+		power_level: 800
+	},
 ]
 
 // ----------------------------------------------------------
@@ -101,15 +101,11 @@ const characters = [
 // Create an array containing only the names of Captains from all universes.
 
 // Your Code here
-const captains = characters.filter(x => {
-  return x.role === 'Captain';
-});
+const captains = characters
+	.filter(c => c.role === 'Captain')
+	.map(c => c.name);
 
-const captainNames = captains.map(x => {
-  return x.name;
-});
-
-console.log(captainNames);
+console.log(captains);
 // expected output: ['Mal Reynolds', 'Kathryn Janeway']
 
 // ----------------------------------------------------------
@@ -121,78 +117,83 @@ console.log(captainNames);
 // Group all characters by universe in a multidimensional array
 
 // Your Code here
+const universeGroup = characters.reduce((group, character) => {
+	group[character.universe] = [...group[character.universe] || [], character];
+	return group;
+}, {});
 
+console.log(universeGroup);
 // expected output:
 
 // [ 
-//  [ 
-//    { name: 'Marvin the Paranoid Android',
-//       role: 'First Mate',
-//       universe: 'Hitchhikers Guide to the Galaxy',
-//       weapon: 'severe depression',
-//       power_level: 1000 
-//     } 
-//   ],
-//   [ { name: 'Jabba the Hut',
-//       role: 'villain',
-//       universe: 'Star Wars',
-//       weapon: 'henchmen',
-//       power_level: 200 },
-//     { name: 'Boba Fett',
-//       role: 'Bounty Hunter',
-//       universe: 'Star Wars',
-//       weapon: 'EE-3 carbine rifle',
-//       power_level: 400 },
-//     { name: 'Yoda',
-//       role: 'Jedi Master',
-//       universe: 'Star Wars',
-//       weapon: 'The Force',
-//       power_level: 900 },
-//     { name: 'R2-D2',
-//       role: 'Ship`s Robot',
-//       universe: 'Star Wars',
-//       weapon: 'Data Probe',
-//       power_level: 250 } ],
-//   [ { name: 'Zoë Alleyne Washburne',
-//       role: 'First Mate',
-//       universe: 'Firefly',
-//       weapon: 'Winchester Model 1892 rifle',
-//       power_level: 160 },
-//     { name: 'Mal Reynolds',
-//       role: 'Captain',
-//       universe: 'Firefly',
-//       weapon: 'pistol',
-//       power_level: 160 } ],
-//   [ { name: 'Peter Venkman',
-//       role: 'Ghostbuster',
-//       universe: 'Ghostbusters',
-//       weapon: 'proton pack',
-//       power_level: 120 } ],
-//   [ { name: 'Kathryn Janeway',
-//       role: 'Captain',
-//       universe: 'Star Trek',
-//       weapon: 'Wit',
-//       power_level: 140 },
-//     { name: 'Q',
-//       role: 'God/Eternal',
-//       universe: 'Star Trek',
-//       weapon: 'Whatever he wants',
-//       power_level: 1000 },
-//     { name: 'Spock',
-//       role: 'First Mate',
-//       universe: 'Star Trek',
-//       weapon: 'Logic',
-//       power_level: 170 },
-//     { name: 'Lore',
-//       role: 'Villain',
-//       universe: 'Star Trek',
-//       weapon: 'Intellect',
-//       power_level: 800 } ],
-//   [ { name: 'Dr. Daniel Jackson',
-//       role: 'Archeologist',
-//       universe: 'Stargate',
-//       weapon: 'Zat gun',
-//       power_level: 120 } ] ]
+//	[ 
+//		{ name: 'Marvin the Paranoid Android',
+//			 role: 'First Mate',
+//			 universe: 'Hitchhikers Guide to the Galaxy',
+//			 weapon: 'severe depression',
+//			 power_level: 1000 
+//		 } 
+//	 ],
+//	 [ { name: 'Jabba the Hut',
+//			 role: 'villain',
+//			 universe: 'Star Wars',
+//			 weapon: 'henchmen',
+//			 power_level: 200 },
+//		 { name: 'Boba Fett',
+//			 role: 'Bounty Hunter',
+//			 universe: 'Star Wars',
+//			 weapon: 'EE-3 carbine rifle',
+//			 power_level: 400 },
+//		 { name: 'Yoda',
+//			 role: 'Jedi Master',
+//			 universe: 'Star Wars',
+//			 weapon: 'The Force',
+//			 power_level: 900 },
+//		 { name: 'R2-D2',
+//			 role: 'Ship`s Robot',
+//			 universe: 'Star Wars',
+//			 weapon: 'Data Probe',
+//			 power_level: 250 } ],
+//	 [ { name: 'Zoë Alleyne Washburne',
+//			 role: 'First Mate',
+//			 universe: 'Firefly',
+//			 weapon: 'Winchester Model 1892 rifle',
+//			 power_level: 160 },
+//		 { name: 'Mal Reynolds',
+//			 role: 'Captain',
+//			 universe: 'Firefly',
+//			 weapon: 'pistol',
+//			 power_level: 160 } ],
+//	 [ { name: 'Peter Venkman',
+//			 role: 'Ghostbuster',
+//			 universe: 'Ghostbusters',
+//			 weapon: 'proton pack',
+//			 power_level: 120 } ],
+//	 [ { name: 'Kathryn Janeway',
+//			 role: 'Captain',
+//			 universe: 'Star Trek',
+//			 weapon: 'Wit',
+//			 power_level: 140 },
+//		 { name: 'Q',
+//			 role: 'God/Eternal',
+//			 universe: 'Star Trek',
+//			 weapon: 'Whatever he wants',
+//			 power_level: 1000 },
+//		 { name: 'Spock',
+//			 role: 'First Mate',
+//			 universe: 'Star Trek',
+//			 weapon: 'Logic',
+//			 power_level: 170 },
+//		 { name: 'Lore',
+//			 role: 'Villain',
+//			 universe: 'Star Trek',
+//			 weapon: 'Intellect',
+//			 power_level: 800 } ],
+//	 [ { name: 'Dr. Daniel Jackson',
+//			 role: 'Archeologist',
+//			 universe: 'Stargate',
+//			 weapon: 'Zat gun',
+//			 power_level: 120 } ] ]
 
 // ----------------------------------------------------------
 
@@ -203,7 +204,13 @@ console.log(captainNames);
 // Create an array containing characters' names who are the only character listed in their universe.
 
 // Your Code here
+const oneUniverse = characters
+	.filter(x => characters
+		.filter(y => y.universe === x.universe)
+	.length === 1)
+	.map(z => z.name);
 
+console.log(oneUniverse);
 // expected output: [ Marvin the Paranoid Android, Peter Venkman, Dr. Daniel Jackson ]
 
 // ----------------------------------------------------------
@@ -215,5 +222,10 @@ console.log(captainNames);
 // What is the average power level across all characters?
 
 // Your code here
+const averagePowerLevel = characters
+	.map(c => c.power_level)
+	.reduce((total, current) => total + current) / characters.length;
 
-// expected output: 68.71319452795147
+console.log(averagePowerLevel);
+// expected output: 68.71319452795147 -- this was provided but is inaccurate
+// actual expected output: 416.9230769230769
