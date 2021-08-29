ecf955969e71aa748b7029b03f8da03f9059a078
b--bell <babell1302@gmail.com>
2021-08-29 14:01:29 -0500

    feat: Update DOM elements uring functional methodologies

--- fp_in_js/application_state_exercise/application_state.js
+++ fp_in_js/application_state_exercise/application_state.js
@@ -8,27 +8,32 @@ let store = {
 }
 
 const render = (root, state) => {
-    root.innerHTML = App(state)
+    root.innerHTML = App(state);
 }
 
 const Welcome = (name) => {
-    return `Welcome, ${name} to my Javascript Program!`
+    return `Welcome, ${name} to my Javascript Program!`;
 }
 
 const App = (state) => {
     return `
        <h1>${Welcome(state.user.first_name)}<h1>
       <div> I EXIST! </div>
-   `
+   `;
 }
 
 window.addEventListener('load', () => {
-    render(root, store)
+    render(root, store);
 })
 
 // But what if state changes at some point in our app?
 // Using the object methods we have learned so far in this course, 
-// create a function called updateStore that takes in the old state, the new state, and updates the old state with any new information
+// create a function called updateStore that takes in the old state, the new state,
+// and updates the old state with any new information
 // This won't be a pure function, instead of a return, call the render method again
 
 // Your Code
+const updateStore = (store, newState) => {
+    store = Object.assign(store, newState);
+    render(root, store);
+}
