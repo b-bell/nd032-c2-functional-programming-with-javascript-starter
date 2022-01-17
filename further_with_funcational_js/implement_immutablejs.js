0b957b0555854f39d22a05d80075bdabec9e89e3
b_bell <Brian.Bell@dell.org>
2022-01-17 15:57:07 -0600

    feat: Add implement ImmutableJS exercise

--- further_with_funcational_js/implement_immutablejs.js
+++ further_with_funcational_js/implement_immutablejs.js
@@ -4,14 +4,23 @@
 // The original code is provided below, how might you re-write both the store object and updateStore function with Immutable?
 // Remember if you need help, the docs should be the first place you look.
 
-let store = {
-    user: {
+let store = Immutable.Map({
+    user: Immutable.Map({
         first_name: 'John',
         last_name: 'Doe'
-    }
-}
+    })
+});
 
+/*
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
     render(root, store)
+}*/
+
+function updateStore(state, newState) {
+    store = state.merge(newState);
+    render(root, store);
 }
+
+updateStore(store, newState);
+console.log(store.getIn(['user', 'name']));
