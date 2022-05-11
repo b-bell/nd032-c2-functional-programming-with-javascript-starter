dffd3cffd6ced76e53c5dd8a4b9dc9d42562765a
Brian Bell <iambrianbell@gmail.com>
2022-05-10 22:52:35 -0500

    feat: Async challenges

--- example/asynchronous_hello_world.js
+++ example/asynchronous_hello_world.js
@@ -8,18 +8,67 @@ const printHelloWorld = () => {
 	setTimeout(console.log, 2000, "World");
 
 	console.log("Hello ");
-}
+};
 
 printHelloWorld();
 
-// CHALLENGE - Move the setTimeout after the console log, does that change the outcome? 
+// CHALLENGE - Move the setTimeout after the console log, does that change the outcome?
+
+const printHelloWorld2 = () => {
+	console.log("Hello ");
+	setTimeout(() => {
+		console.log("World")
+	}, 2000);
+};
+
+printHelloWorld2();
 
 // CHALLENGE - Edit the time elapsed between "Hello" and "World" appearing on the screen
+
+const printHelloWorld3 = () => {
+	console.log("Hello ");
+	setTimeout(() => {
+		console.log("World");
+	}, 5000);
+};
+
+printHelloWorld3();
 
 // CHALLENGE - Edit the message being sent to have three parts:
 // Part 1: "I'm " - shows up immediately
 // Part 2: "async" - shows up 2 seconds after "I'm"
 // Part 3: "...ronous!" - shows up 4 seconds after "async"
 
+const printHelloWorld4 = () => {
+	console.log("I'm ");
+	setTimeout(() => {
+		console.log("async");
+	}, 2000);
+	setTimeout(() => {
+		console.log("...hronous!");
+	}, 6000);
+};
+
+printHelloWorld4();
+
 // Extra Challenge - if you want to go even further with it, time each of the dots in the ellipses of part 3
 // Write a function to make each dot appear one second after the other.
+
+const printHelloWorld5 = () => {
+	console.log("I'm ");
+	setTimeout(() => {
+		console.log("async");
+	}, 2000);
+	let i = 0;
+	while(i<3) {
+		setTimeout(() => {
+			console.log(".");
+		}, (i+1) * 1000);
+		i++;
+	}
+	setTimeout(() => {
+		console.log("hronous!");
+	}, 6000);
+};
+
+printHelloWorld5();
